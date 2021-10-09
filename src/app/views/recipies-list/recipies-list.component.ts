c20dd6570efc1ae81958f43354092c18e01b8138
Marcin <skrzyczewski.marcin@gmail.com>
2021-10-09 19:56:15 +0200

    added recipes table

--- src/app/views/recipies-list/recipies-list.component.ts
+++ src/app/views/recipies-list/recipies-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { RecipeAddEditModel, RecipiesService } from 'src/app/services/recipies/recipies.service';
 
 @Component({
   selector: 'mb-recipies-list',
   templateUrl: './recipies-list.component.html',
-  styleUrls: ['./recipies-list.component.scss']
+  styleUrls: ['./recipies-list.component.scss'],
 })
 export class RecipiesListComponent implements OnInit {
+  recipes: RecipeAddEditModel[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private recipiesService: RecipiesService,
+    private router: Router) {
+    this.recipes = [
+      {
+        name: 'Duel Master Black IPA',
+        id: 1,
+        litres: 10,
+        ingredients: ['water', 'barley'],
+        stages: [
+          { name: 'a', days: 1 },
+          { name: 'a', days: 1 },
+        ],
+      },
+    ];
   }
 
+  ngOnInit(): void {}
 }
